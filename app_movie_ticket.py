83e0843b82c988f795128532fce9dfbae1d509a3
ZhiXian03 <zxiancheong03@gmail.com>
2026-04-06 14:47:05 +0800

    Add files via upload

---
+++ app_movie_ticket.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+st.title("Movie Ticket Booking System")
+
+customer_name = st.text_input("Enter Customer Name")
+
+movie_title = st.selectbox("Select Movie Title", ["Avangers", "Kung Fu Panda", "Frozen"])
+
+show_time = st.selectbox("Select Show Time", ["10:00 AM", "2:00 PM", "8:00 PM"])
+
+seat_type = st.radio("Select Seat Type", ["Standard", "Premium"])
+
+
+if st.button("Book Ticket"):
+    
+    try:
+        if customer_name.strip() == "":
+            raise ValueError("Customer name cannot be empty!")
+
+        st.success("Ticket booked successfully!")
+
+        st.subheader("*****Booking Details*****")
+        st.write(f"Customer Name: {customer_name}")
+        st.write(f"Movie Title: {movie_title}")
+        st.write(f"Show Time: {show_time}")
+        st.write(f"Seat Type: {seat_type}")
+
+    except ValueError as ve:
+        st.error(f"Input Error: {ve}")
+
+    except Exception as e:
+        st.error(f"System Error: {e}")
